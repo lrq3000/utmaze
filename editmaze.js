de09f53f36e21c1d2805d5bb2f11462e77b68a7f
Logan "CodeWeaver" Hall <codeweaver@loganhall.net>
2022-06-22 14:52:02 -0500

    Implemented filling in tiles

--- editmaze.js
+++ editmaze.js
@@ -1,10 +1,14 @@
 var maze = emptyMaze(16,10);
 var brush = TILE.NONE;
 
-document.addEventListener('click', e => {
-	maze[Math.floor(e.clientX / b)][Math.floor(e.clientY / b)] = brush;
+var canvas = document.getElementById('canvas');
+function paintTile(e) {
+	const rect = canvas.getBoundingClientRect();
+	maze[Math.floor((e.clientX - rect.left) / b)][Math.floor((e.clientY - rect.top) / b)] = brush;
 	drawMaze(maze);
-});
+}
+canvas.addEventListener('mousedown', e => { paintTile(e) });
+canvas.addEventListener('mousemove', e => { if (e.buttons == 1) paintTile(e) });
 
 document.addEventListener('keydown', e => { if (!isNaN(e.key)) brush = parseInt(e.key) });
 
