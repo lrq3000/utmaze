6021a2c5b8c45b9a13794e93d892f52616d25cae
Logan "CodeWeaver" Hall <codeweaver@loganhall.net>
2022-06-23 00:37:09 -0500

    Base64 encoding & decoding for maze data

--- defs.js
+++ defs.js
@@ -20,7 +20,6 @@ var b = 40;	// Breadth of tile (Size in px)
 
 function randInt(n) { return Math.floor(Math.random() * n); }
 
-/* Initialize maze 2D array */
 function emptyMaze (w,h) {
 	let maze = new Array(w);
 	for (let i=0; i<w; i++)
@@ -43,4 +42,38 @@ function drawMaze(maze) {
 	}
 }
 
+let b64s = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+function base64(maze) {
+	let b64 = '';
+	let tileBuf = [];
 
+	// Load tiles into 1-dimensional buffer
+	for (let x=0; x<maze.length; x++)
+		tileBuf = tileBuf.concat(maze[x]);
+
+	// 6 bits per character base64-encoded, 3 bits per tile
+	for (let i=0; i<tileBuf.length; )
+		b64 += b64s.charAt((tileBuf[i++] << 3) | tileBuf[i++]);
+
+	// Encode maze height (max 64)
+	return b64s.charAt(maze[0].length-1)+b64;
+}
+function load(base64) {
+	let h = 1 + b64s.indexOf(base64.charAt(0));
+	let w = (base64.length - 1) * 2 / h;	// w*h=a, a/h=w
+	let maze = emptyMaze(w,h);
+
+	let tileBuf = [];
+	for (let i=1; i<base64.length; i++) {
+		let octets = b64s.indexOf(base64.charAt(i));
+		tileBuf.push(octets >>> 3);
+		tileBuf.push(octets & 0b111);
+	}
+
+	let i=0;
+	for (let x=0; x<w; x++)
+		for (let y=0; y<h; y++)
+			maze[x][y] = tileBuf[i++];
+
+	return maze;
+}
