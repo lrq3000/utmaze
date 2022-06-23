bf1ba5ea91ad48b4ca9af59d8ae8f69adbd08a76
Logan "CodeWeaver" Hall <codeweaver@loganhall.net>
2022-06-23 00:37:18 -0500

    Editor controls

--- editmaze.js
+++ editmaze.js
@@ -1,15 +1,27 @@
-var maze = emptyMaze(16,10);
+var maze = emptyMaze(8,6);
 var brush = TILE.NONE;
 
 var canvas = document.getElementById('canvas');
+var codebar = document.getElementById('levelcode');
+
+function tick() {
+	drawMaze(maze);
+	codebar.value = base64(maze);
+}
+
+function initMaze() {
+	maze = emptyMaze(document.getElementById('width').value, document.getElementById('height').value);
+	tick();
+}
+
 function paintTile(e) {
 	const rect = canvas.getBoundingClientRect();
 	maze[Math.floor((e.clientX - rect.left) / b)][Math.floor((e.clientY - rect.top) / b)] = brush;
-	drawMaze(maze);
+	tick();
 }
-canvas.addEventListener('mousedown', e => { paintTile(e) });
+canvas.addEventListener('mousedown', e => { /*e.preventDefault();*/ paintTile(e) });
 canvas.addEventListener('mousemove', e => { if (e.buttons == 1) paintTile(e) });
 
 document.addEventListener('keydown', e => { if (!isNaN(e.key)) brush = parseInt(e.key) });
 
-drawMaze(maze);
+tick();
