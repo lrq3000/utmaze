370cafabefdfd5b2be601740b84506733be6d4f4
Logan "CodeWeaver" Hall <codeweaver@loganhall.net>
2022-06-30 12:22:55 -0500

    Added some comments

--- defs.js
+++ defs.js
@@ -7,8 +7,8 @@ const TILE = {
 	BLUE:	5,
 	YELLOW:	6,
 	RED:	7,
-	PLAID:	8,
-	ELEC:	9
+	PLAID:	8,	// Start and End tile
+	ELEC:	9	// Electrified blue tile
 };
 const COLOR = ['Black','#FEBFC0','#FFC049','#C000C0','#9BFF78','#4040FF','#FFFF80','#FF3F3F','#555555','#8080FF'];
 const STATE = {
