a98c22ec6b28c93f30782d36c88a9a1602dfb6bf
Logan "CodeWeaver" Hall <codeweaver@loganhall.net>
2022-06-30 00:45:22 -0500

    Player is a heart now

--- Draw.js
+++ Draw.js
@@ -1,34 +1,30 @@
 const b = 40;	// Breadth of tile (Size in px)
 
 function drawMaze(maze) {
-        let canvas = document.getElementById('canvas');
-        let ctx = canvas.getContext('2d');
+	let canvas = document.getElementById('canvas');
+	let ctx = canvas.getContext('2d');
 
-        ctx.clearRect(0,0,canvas.width,canvas.height);
+	ctx.clearRect(0,0,canvas.width,canvas.height);
 
-        for (let x=0; x<maze.length; x++) {
-                for (let y=0; y<maze[0].length; y++) {
-                        ctx.beginPath();
-                        ctx.rect(b*x,b*y,b,b);
-                        ctx.fillStyle = (COLOR[maze[x][y]] || 'black');
-                        ctx.fill();
-                        ctx.closePath();
-                }
-        }
+	for (let x=0; x<maze.length; x++) {
+		for (let y=0; y<maze[0].length; y++) {
+			ctx.beginPath();
+			ctx.rect(b*x,b*y,b,b);
+			ctx.fillStyle = (COLOR[maze[x][y]] || 'black');
+			ctx.fill();
+			ctx.closePath();
+		}
+	}
 }
 
+const heart = new Image();
+heart.src = 'heart.png';
 function drawPlayer(player) {
 	let canvas = document.getElementById('canvas');
-        let ctx = canvas.getContext('2d');
+	let ctx = canvas.getContext('2d');
 
 	let x = player.x;
 	let y = player.y;
 
-        ctx.beginPath();
-        ctx.arc(x*b+(b/2), y*b+(b/2), b/3, 0, 2*Math.PI, false);
-        ctx.fillStyle = 'white';
-        ctx.strokeStyle = 'black';
-        ctx.lineWidth = 2;
-        ctx.fill();
-        ctx.stroke();
+	ctx.drawImage(heart,(x*b)+(b*0.125),(y*b)+(b*0.125),b*0.75,b*0.75);
 }
