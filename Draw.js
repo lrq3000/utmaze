986afc27f406b31f1daca8b1794b989c40f5e16c
Logan "CodeWeaver" Hall <codeweaver@loganhall.net>
2022-07-01 03:11:24 -0500

    Added several graphics, moved all graphics to their own folder

--- Draw.js
+++ Draw.js
@@ -1,5 +1,7 @@
 const b = 40;	// Breadth of tile (Size in px)
 
+const plaid = new Image();
+plaid.src = 'graphics/plaid.png';
 function drawMaze(maze) {
 	let canvas = document.getElementById('canvas');
 	let ctx = canvas.getContext('2d');
@@ -8,17 +10,22 @@ function drawMaze(maze) {
 
 	for (let x=0; x<maze.length; x++) {
 		for (let y=0; y<maze[0].length; y++) {
-			ctx.beginPath();
-			ctx.rect(b*x,b*y,b,b);
-			ctx.fillStyle = (COLOR[maze[x][y]] || 'black');
-			ctx.fill();
-			ctx.closePath();
+			let tile = maze[x][y];
+			if (tile == TILE.PLAID)
+				ctx.drawImage(plaid,b*x,b*y,b,b);
+			else {
+				ctx.beginPath();
+				ctx.rect(b*x,b*y,b,b);
+				ctx.fillStyle = (COLOR[tile] || 'black');
+				ctx.fill();
+				ctx.closePath();
+			}
 		}
 	}
 }
 
 const heart = new Image();
-heart.src = 'heart.png';
+heart.src = 'graphics/heart.png';
 function drawPlayer(player) {
 	let canvas = document.getElementById('canvas');
 	let ctx = canvas.getContext('2d');
@@ -26,5 +33,5 @@ function drawPlayer(player) {
 	let x = player.x;
 	let y = player.y;
 
-	ctx.drawImage(heart,(x*b)+(b*0.125),(y*b)+(b*0.125),b*0.75,b*0.75);
+	ctx.drawImage(heart,(b*x)+(b*0.125),(b*y)+(b*0.125),b*0.75,b*0.75);
 }
